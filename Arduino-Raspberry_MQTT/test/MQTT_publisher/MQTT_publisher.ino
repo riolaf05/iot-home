57cdaafc64037eaa681ed85d983403c0bf06a1bc
rosario89 <lafacerosario@gmail.com>
2019-05-12 13:47:24 +0200

    changing file name

--- Arduino-Raspberry_MQTT/test/MQTT_publisher/MQTT_publisher.ino
+++ Arduino-Raspberry_MQTT/test/MQTT_publisher/MQTT_publisher.ino
@@ -1,12 +1,15 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
- 
-const char* ssid = "yourNetworkName";
-const char* password =  "yourNetworkPassword";
-const char* mqttServer = "m11.cloudmqtt.com";
-const int mqttPort = 12948;
-const char* mqttUser = "yourMQTTuser";
-const char* mqttPassword = "yourMQTTpassword";
+
+// WiFi
+const char* ssid = "Vodafone-A41502247";
+const char* wifi_password =  "s3wv93bx9pkwd3m5";
+// MQTT
+// TODO: externalize parameters!!!
+const char* mqttServer = "192.168.1.9";
+const int mqttPort = 1883;
+const char* mqttUser = "rio";
+const char* mqttPassword = "onslario89";
  
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -14,8 +17,9 @@ PubSubClient client(espClient);
 void setup() {
  
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
- 
+  WiFi.begin(ssid, wifi_password);
+
+  //Wi-Fi Connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
@@ -24,7 +28,8 @@ void setup() {
   Serial.println("Connected to the WiFi network");
  
   client.setServer(mqttServer, mqttPort);
- 
+
+  //MQTT Broker Connection
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
  

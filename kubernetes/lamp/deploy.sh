863323a87e1901578b8257ffa7dfee225d3cc5d6
Rosario Laface <rosario.laface@nttdata.com>
2019-05-27 17:14:11 +0200

    removing lamp k8s resources

--- kubernetes/lamp/deploy.sh
+++
@@ -1,9 +0,0 @@
-#Cronjobs for external jobs
-
-
-#Kubernetes jobs
-kubectl apply -f persistantVolume.yaml
-kubectl apply -f persistantVolumeClaim.yaml
-kubectl apply -f secrets.yaml
-kubectl apply -f php.yaml
-kubectl apply -f ingress.yaml

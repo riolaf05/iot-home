773bb2c95a2aacc584e35d26cf727e96f608c49b
rosario89 <lafacerosario@gmail.com>
2019-07-20 11:29:48 +0200

    Update MQTT_Client.py

--- Arduino-Raspberry_MQTT/python/mqtt-publisher-client/mqtt-publisher-generic/MQTT_Client.py
+++ Arduino-Raspberry_MQTT/python/mqtt-publisher-client/mqtt-publisher-generic/MQTT_Client.py
@@ -1,27 +1,22 @@
-
-import time
 import paho.mqtt.client as paho
-
+#see: http://www.steves-internet-guide.com/publishing-messages-mqtt-client/
 broker="rpi-mosquitto"
-#broker="iot.eclipse.org" #TODO: add external MQTT broker
+port=1883
 
-#define callback
-def on_message(client, userdata, message):
-    time.sleep(1)
-    print("received message =",str(message.payload.decode("utf-8")))
+def on_publish(client,userdata,result):             #create function for callback. The mid value is the message id and can 
+													#be used with the mid value returned from the publish method to check that 
+													#a particular message has been published.
+    print("data published \n")
+    pass
 
-client= paho.Client("IoT-device-001") #create client object client1.on_publish = on_publish #assign function to callback client1.connect(broker,port) #establish connection client1.publish("house/bulb1","on")
-######Bind function to callback
-client.on_message=on_message
-#####
-print("connecting to broker ",broker)
-client.connect(broker)#connect
-client.loop_start() #start loop to process received messages
-#print("subscribing ")
-#client.subscribe("test")#subscribe
-#time.sleep(2)
-print("publishing ")
-client.publish("test","message")#publish
-time.sleep(4)
-client.disconnect() #disconnect
-client.loop_stop() #stop loop
+def on_disconnect(client, userdata, rc):
+   print("client disconnected ok")
+
+client1= paho.Client("control1")                    #create client object
+client1.on_publish = on_publish                     #assign function to callback
+#client.tls_set('c:/python34/steve/MQTT-demos/certs/ca.crt') #to set SSL certificate
+client1.connect(broker,port)                        #establish connection
+ret= client1.publish("test","10")            		#publish
+
+client1.on_disconnect = on_disconnect
+client1.disconnect()

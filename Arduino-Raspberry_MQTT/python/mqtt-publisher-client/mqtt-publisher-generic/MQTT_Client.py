d5855a6d1434d9af4e4e2a6006fd2f06ec01882d
rosario89 <lafacerosario@gmail.com>
2019-07-20 12:51:03 +0200

    mqtt publish - changes on client and cronjob

--- Arduino-Raspberry_MQTT/python/mqtt-publisher-client/mqtt-publisher-generic/MQTT_Client.py
+++ Arduino-Raspberry_MQTT/python/mqtt-publisher-client/mqtt-publisher-generic/MQTT_Client.py
@@ -12,11 +12,19 @@ def on_publish(client,userdata,result):             #create function for callbac
 def on_disconnect(client, userdata, rc):
    print("client disconnected ok")
 
+def on_log(client, userdata, level, buf):
+    print("log: ",buf)
+
 client1= paho.Client("control1")                    #create client object
+client1.on_log=on_log
 client1.on_publish = on_publish                     #assign function to callback
+
 #client.tls_set('c:/python34/steve/MQTT-demos/certs/ca.crt') #to set SSL certificate
+
+print("connectiong to broker..")
 client1.connect(broker,port)                        #establish connection
-ret= client1.publish("test","10")            		#publish
+ret= client1.publish("test","10")
+print("message published..")            		#publish
 
 client1.on_disconnect = on_disconnect
 client1.disconnect()

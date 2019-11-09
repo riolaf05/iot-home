c33cd5340c31ffd2ebd68e264e8c7041eea3739f
riolaf05 <30504264+riolaf05@users.noreply.github.com>
2019-11-09 12:55:03 +0100

    Update MQTT_publisher_subscriber_moisture_sensor_relay_without_power_s.ino

--- Arduino-Raspberry_MQTT/devices/MQTT_publisher_subscriber_moisture_sensor_relay_without_power_s/MQTT_publisher_subscriber_moisture_sensor_relay_without_power_s.ino
+++ Arduino-Raspberry_MQTT/devices/MQTT_publisher_subscriber_moisture_sensor_relay_without_power_s/MQTT_publisher_subscriber_moisture_sensor_relay_without_power_s.ino
@@ -1,4 +1,3 @@
-#include <Bounce2.h> // Used for "debouncing" the pushbutton
 #include <ESP8266WiFi.h> // Enables the ESP8266 to connect to the local network (via WiFi)
 #include <PubSubClient.h> // Allows us to connect to, and publish to the MQTT broker
 

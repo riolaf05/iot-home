a43c4c4403242fa720fff4142cffc46e4de7ed36
riolaf05 <30504264+riolaf05@users.noreply.github.com>
2019-11-14 22:49:26 +0100

    adding temperature sensor subscriber

--- Arduino-Raspberry_MQTT/python/mqtt-subscriber-client/mqtt-subscriber-temperature/MQTT_Client.py
+++ Arduino-Raspberry_MQTT/python/mqtt-subscriber-client/mqtt-subscriber-temperature/MQTT_Client.py
@@ -4,11 +4,11 @@ import urllib2
 import time
 import datetime
 from time import sleep
-from on_messages import on_message_test
+from on_messages import on_message
 #import Adafruit_DHT as dht
 
 # Enter Your ThingSpeak API key here
-myAPI = 'CTPSMGYK3QGWKV8T'
+myAPI = 'JHTAY1BANPFEPCW0'
 # URL where we will send the data, Don't change it
 baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
 
@@ -17,16 +17,16 @@ def on_subscribe(client, userdata, mid, granted_qos):
 
 def on_message(client, userdata, msg):
         try:
-                humi = float(msg.payload)
+                temperature = float(msg.payload)
                 # If Reading is valid
-                if isinstance(humi, float):
+                if isinstance(temperature, float):
                         # Formatting to two decimal places
-                        humi = '%.2f' % humi                                    
+                        temperature = '%.2f' % temperature                                    
                         # Sending the data to ThingSpeak
-                        conn = urllib2.urlopen(baseURL + '&field1=%s' % (humi))
+                        conn = urllib2.urlopen(baseURL + '&field1=%s' % (temperature))
                         ts = time.time()
                         st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-                        print str(st) + ": humidity " + str(humi) + "%"
+                        print str(st) + ": temperature " + str(temperature) + "° C"
                         # Closing the connection
                         conn.close()
                 else:
@@ -40,12 +40,13 @@ def on_message(client, userdata, msg):
 This script is used to listen on MQTT port 1883 and trigger the "on_message_xxx" methods
 which actually publishes on ThingSpeak or other front end objects.  
 '''
+
 client = paho.Client()
-client.username_pw_set("rio", "onslario89")
+#client.username_pw_set("rio", "onslario89")
 client.on_subscribe = on_subscribe
 client.on_message = on_message
 try:
-        client.connect("rpi-mosquitto", 1883)
+        client.connect("192.168.1.10", 30050) #TODO: fix hard coded endpoint IP #old ip 10.44.0.5?
         client.subscribe("temperature", qos=1)
         print "connecting.."
 

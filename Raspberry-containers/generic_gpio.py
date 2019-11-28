486e146daca5e60760d8de68dc18daf28f032c9c
Rosario Laface <rosario.laface@nttdata.com>
2019-11-28 16:24:47 +0100

    adding dockerfile and script

---
+++ Raspberry-containers/generic_gpio.py
@@ -0,0 +1,22 @@
+#See https://iotbytes.wordpress.com/create-your-first-docker-container-for-raspberry-pi-to-blink-an-led/
+
+import RPi.GPIO as GPIO
+import time
+
+# Configure the PIN # 8
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT)
+GPIO.setwarnings(False)
+
+# Blink Interval 
+blink_interval = .5 #Time interval in Seconds
+
+# Blinker Loop
+while True:
+ GPIO.output(8, True)
+ time.sleep(blink_interval)
+ GPIO.output(8, False)
+ time.sleep(blink_interval)
+
+# Release Resources
+GPIO.cleanup()

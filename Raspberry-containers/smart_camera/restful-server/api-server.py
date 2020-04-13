60a5307bb130e35e7892853ee2bdf7c32f291aaf
riolaf05 <lafacerosario@gmail.com>
2020-04-13 16:04:23 +0200

    Update api-server.py

--- Raspberry-containers/smart_camera/restful-server/api-server.py
+++ Raspberry-containers/smart_camera/restful-server/api-server.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.insert(1, '/usr/lib/python3/dist-packages', '/home/pi/.local/lib/python3.7/site-packages')
+sys.path.insert(1, '/usr/lib/python3/dist-packages')
+sys.path.insert(1, '/home/pi/.local/lib/python3.7/site-packages')
 
 from flask import Flask, Response, request
 import subprocess

58b3670681d185d4693866db1bb694d37d6cdce6
riolaf05 <lafacerosario@gmail.com>
2020-05-09 17:36:15 +0200

    update mqtt generic sub

--- Arduino-Raspberry_MQTT/devices/MQTT_subscriber_generic/MQTT_subscriber_generic.ino
+++ Arduino-Raspberry_MQTT/devices/MQTT_subscriber_generic/MQTT_subscriber_generic.ino
@@ -1,21 +1,28 @@
-#include <Bounce2.h> // Used for "debouncing" the pushbutton
 #include <ESP8266WiFi.h> // Enables the ESP8266 to connect to the local network (via WiFi)
 #include <PubSubClient.h> // Allows us to connect to, and publish to the MQTT broker
+#include <DHT.h>        // DHT11 temperature and humidity sensor Predefined library
+
+#define DHTTYPE DHT22
+#define PUMP D7
+#define dht_dpin D3
 
 const int ledPin = 0; // This code uses the built-in led for visual feedback that the button has been pressed
 const int buttonPin = 13; // Connect your button to pin #13
+float t =0.0;
 
 // WiFi
 // Make sure to update this for your own WiFi network!
-const char* ssid = "Vodafone-A41502247";
-const char* wifi_password = "s3wv93bx9pkwd3m5";
+const char* ssid = "FASTWEB-D82B93";
+const char* wifi_password = "W1JA3M3R2A";
 
 // MQTT
 // Make sure to update this for your own MQTT Broker!
 // TODO: externalize parameters!!!
 const char* mqtt_server = "192.168.1.0";
-const char* mqtt_topic = "moisture";
+const char* mqtt_moisture_topic = "moisture";
+const char* mqtt_temperature_topic = "temperature";
 const char* mqtt_sub_topic = "pump_activation";
+const char* status_topic = "smart_garden_status";
 const char* mqtt_username = "rio";
 const char* mqtt_password = "onslario89";
 const int mqtt_port = 1883; //choose K8s MQTT port
@@ -25,34 +32,32 @@ const char* ok_message = "ON";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+DHT dht(dht_dpin, DHTTYPE);
+
  
 void callback(char* topic, byte* payload, unsigned int length) {
- 
-  Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
- 
-  Serial.print("Message:");
+  String string = "";
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    //Serial.print((char)payload[i]);
+    string.concat((char)payload[i]);
   }
- 
-  Serial.println();
-  Serial.println("-----------------------");
- 
+  Serial.println(string);
 }
 
+
 void setup() {
- 
+  //configuring Serial, WIFI, outputs
   Serial.begin(115200);
- 
   WiFi.begin(ssid, wifi_password);
- 
+  pinMode(PUMP, OUTPUT);       //D7 as output
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
- 
+  
+  //Configuring MQTT client
+   Serial.println("Configuring MQTT client..");
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
  
@@ -60,22 +65,25 @@ void setup() {
     Serial.println("Connecting to MQTT...");
  
     if (client.connect("ESP32Client", mqtt_username, mqtt_password )) {
- 
       Serial.println("connected");  
- 
     } else {
- 
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
- 
     }
   }
- 
+  
+  //Subscribing to MQTT queue
   client.subscribe("pump_activation");
- 
+  
 }
+
+
+
  
 void loop() {
+  Serial.println("Checking pump activation..");
   client.loop();
+ 
+  delay(5000);
 }

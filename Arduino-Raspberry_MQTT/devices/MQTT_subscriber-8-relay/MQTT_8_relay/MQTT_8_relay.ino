36a3555d262d85925ce7f38284cbed028c79539b
riolaf05 <lafacerosario@gmail.com>
2020-05-31 17:28:47 +0200

    update 8 relay module

--- Arduino-Raspberry_MQTT/devices/MQTT_subscriber-8-relay/MQTT_8_relay/MQTT_8_relay.ino
+++ Arduino-Raspberry_MQTT/devices/MQTT_subscriber-8-relay/MQTT_8_relay/MQTT_8_relay.ino
@@ -8,7 +8,7 @@
 
 const int ledPin = 0; // This code uses the built-in led for visual feedback that the button has been pressed
 const int buttonPin = 13; // Connect your button to pin #13
-float t =0.0;
+float t = 0.0;
 
 // WiFi
 // Make sure to update this for your own WiFi network!
@@ -25,13 +25,13 @@ const char* mqtt_sub_topic = "pump_activation";
 const char* status_topic = "smart_garden_status";
 const char* mqtt_username = "rio";
 const char* mqtt_password = "onslario89";
-const int mqtt_port = 1883; //choose K8s MQTT port
+const int mqtt_port = 1883; //choose MQTT port
 // The client id identifies the ESP8266 device. Think of it a bit like a hostname (Or just a name, like Greg).
 const char* clientID = "ClientID";
 const char* ok_message = "ON";
 
 //relay water pump control
-int RelayControl1 = 13; //D7 
+int RelayControl1 = 13; //D7
 int RelayControl2 = 15; //D8
 int RelayControl3 = 14; //D5
 int RelayControl4 = 4; //D2
@@ -45,39 +45,44 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(dht_dpin, DHTTYPE);
 
- 
+
 void callback(char* topic, byte* payload, unsigned int length) {
-  
+
   String topic_str = String(topic); //onverting from character array pointer to string
   String string = "";
   for (int i = 0; i < length; i++) { //getting received message
     //Serial.print((char)payload[i]);
     string.concat((char)payload[i]);
   }
-  
+
   //pump activation topic
-  if(topic_str.equals(mqtt_sub_topic)){
-    Serial.println("Message received from "+topic_str+" topic, activating pump: "+string);
-    
-    if(string.equals("1")) {
-      digitalWrite(RelayControl1,HIGH);
+  if (topic_str.equals(mqtt_sub_topic)) {
+    Serial.println("Message received from " + topic_str + " topic, activating pump: " + string);
+
+    if (string.equals("1")) {
+      digitalWrite(RelayControl1, LOW);
       delay(5000);
-      digitalWrite(RelayControl1,LOW);
+      digitalWrite(RelayControl1, HIGH);
     }
-    else if(string.equals("2")) {
-      digitalWrite(RelayControl2,HIGH);
+    else if (string.equals("2")) {
+      digitalWrite(RelayControl2, HIGH);
       delay(5000);
-      digitalWrite(RelayControl2,LOW);
-    } 
-    else if(string.equals("3")) {
-      digitalWrite(RelayControl3,HIGH);
+      digitalWrite(RelayControl2, LOW);
+    }
+    else if (string.equals("3")) {
+      digitalWrite(RelayControl3, HIGH);
       delay(5000);
-      digitalWrite(RelayControl3,LOW);
-    } 
-    else if(string.equals("4")) {
-      digitalWrite(RelayControl4,HIGH);
+      digitalWrite(RelayControl3, LOW);
+    }
+    else if (string.equals("4")) {
+      digitalWrite(RelayControl4, HIGH);
       delay(5000);
-      digitalWrite(RelayControl4,LOW);
+      digitalWrite(RelayControl4, LOW);
+    }
+    else if (string.equals("5")) {
+      digitalWrite(RelayControl4, HIGH);
+      delay(5000);
+      digitalWrite(RelayControl4, LOW);
     }
   }
 }
@@ -87,18 +92,38 @@ int mutiplexerReading(boolean A, boolean B, boolean C) {
   digitalWrite(MultiplexerControl4, A);
   digitalWrite(MultiplexerControl3, B);
   digitalWrite(MultiplexerControl2, C);
-  int value = analogRead(A0);
+  float moisture_value = moistureSensor(A0);
+  char cstr[16];
+  //Serial.print("Moisture value: ");
+  //Serial.println(moisture_value);
 
-  return value;
+  return moisture_value;
 }
 
 
+float moistureSensor(char inputPin) {
+  int dryValue = 1023;
+  int wetValue = 320;
+  int friendlyDryValue = 0;
+  int friendlyWetValue = 100;
+  //function which calculates the moisture sensor output value
+  int rawValue = analogRead(inputPin); //Read the analog value
+  Serial.print("Raw value: ");
+  Serial.print(rawValue);
+  int percentage_value = map(rawValue, dryValue, wetValue, friendlyDryValue, friendlyWetValue); //Calibrazione sensore: https://greensense.github.io/Blog/2017/02/17/Arduino-Soil-Moisture-Sensor-Calibration/
+  Serial.print(", moisture pecentage: ");
+  Serial.print(percentage_value);
+  Serial.print("%");
+  Serial.print("\n");
+  return percentage_value;
+}
 
 
 
 void setup() {
   //configuring Serial, WIFI, outputs
   Serial.begin(115200);
+
   WiFi.begin(ssid, wifi_password);
   pinMode(PUMP, OUTPUT);       //D7 as output
   while (WiFi.status() != WL_CONNECTED) {
@@ -106,24 +131,24 @@ void setup() {
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
-  
+
   //Configuring MQTT client
   Serial.println("Configuring MQTT client..");
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
- 
+
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
- 
+
     if (client.connect("ESP32Client", mqtt_username, mqtt_password )) {
-      Serial.println("connected");  
+      Serial.println("connected");
     } else {
       Serial.print("failed with state ");
       Serial.print(client.state());
       delay(2000);
     }
   }
-  
+
   //Subscribing to MQTT queue
   client.subscribe(mqtt_sub_topic);
 
@@ -132,19 +157,13 @@ void setup() {
   pinMode(RelayControl2, OUTPUT);
   //pinMode(RelayControl3, OUTPUT);
   //pinMode(RelayControl4, OUTPUT);
-
-  //Reset relay pins
-  //digitalWrite(RelayControl1,LOW);
-  //digitalWrite(RelayControl2,LOW);
-  //digitalWrite(RelayControl3,LOW);
-  //digitalWrite(RelayControl4,LOW);
 }
 
 
 
 
 void loop() {
-  //1. 
+  //1.
   Serial.println("Checking pump activation..");
   client.loop();
   delay(5000);
@@ -154,7 +173,7 @@ void loop() {
   //sensor 1
   int value_1 = mutiplexerReading(LOW, LOW, LOW);
   //sensor 2
-  int value_2 = mutiplexerReading(LOW, HIGH, LOW);
+  int value_2 = mutiplexerReading(HIGH, HIGH, LOW);
   //sensor 3
   int value_3 = mutiplexerReading(LOW, HIGH, HIGH);
   //sensor 4
@@ -162,13 +181,19 @@ void loop() {
   //sensor 5
   int value_5 = mutiplexerReading(HIGH, LOW, HIGH);
 
-  Serial.println(value_1);
-  Serial.println(value_2);
-  /*
-  //3. Send values through MQTT..
-  if (client.publish(mqtt_analog_topic, value_1)) { //TODO: fix this to sendmultiple values at once
+
+  //3. Send sensor values through MQTT..
+  String moisture_topic_str = String(mqtt_moisture_topic);
+  int values[]={value_1, value_2, value_3, value_4, value_5};
+  for (int i=0; i<=4; i++) {
+    String moisture_topic=moisture_topic_str+"/"+i;
+    char cstr[16];
+    if (client.publish(moisture_topic.c_str(), itoa(values[i], cstr, 10))) { 
+    }
+    else {
+    Serial.println("Message failed to send.");
+    //}
+    }  
   }
-  else {
-    Serial.println("Message failed to send."); 
-  */
+
 }
